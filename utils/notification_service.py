ede69ef5b1b01f8c132294bc71b35a51695582c5
ydshieh <ydshieh@users.noreply.github.com>
2023-03-04 14:05:03 +0100

    Run CI with PyTorch 2.0

--- utils/notification_service.py
+++ utils/notification_service.py
@@ -910,7 +910,8 @@ if __name__ == "__main__":
 
             if failed:
                 for line in artifact["summary_short"].split("\n"):
-                    if re.search("FAILED", line):
+                    # if re.search("FAILED", line):
+                    if line.strip().startswith("FAILED "):
                         line = line.replace("FAILED ", "")
                         line = line.split()[0].replace("\n", "")
 
